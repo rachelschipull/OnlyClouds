16edef30708e0c4f047982b1c2f8bd50ace9177a
Rachel Schipull <rlschipull@gmail.com>
2022-08-18 14:17:02 -0400

    add updates

--- server.js
+++ server.js
@@ -1,32 +1,21 @@
 const express = require("express")
 const app = express()
+const env = require('dotenv')
+env.config()
+const cors = require('cors')
+const PORT = process.env.PORT || 8000
+const DB = require('./config/config.js')
 
-const admin = require('firebase-admin')
-const credentials = require('./key.json')
-
-admin.initializeApp({
-    credential: admin.credential.cert(credentials)
-})
-
-const db = admin.firestore()
-
+app.use(cors())
 app.use(express.json())
-
 app.use(express.urlencoded({extended: true}))
 
-// app.post('/create', async(req, res) => {
-//     try {
-//         const id = req.body.data;
-//         const userJson = {
-//             email: req.body.email,
-//             firstname: req.body.firstName,
-//             lastName: req.body.lastName
-//     };
-//     const response = db.collection('users').doc(id)
-//     }
-// })
+const homeRoute = require('.routes/home.js')
+const cloudRoute = require('./routes/clouds.js')
 
-const PORT = process.env.PORT || 8080
+app.use('/', homeRoute)
+app.use('/api/clouds/', cloudRoute)
+
 app.listen(PORT, () => {
     console.log(`Server is running on Port ${PORT}`)
 })
